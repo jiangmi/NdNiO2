Modify d8 ground state calculations and fix d9 calculation issues

d8_1eh_pair_impurity_model/ground_state_eigsh.py
@@ -58,15 +58,23 @@ def get_ground_state(matrix, VS, S_val,Sz_val):
             
         print ('eigenvalue = ', vals[k])
         indices = np.nonzero(abs(vecs[:,k])>0.01)
-        wgt_d8 = np.zeros(6)
-        wgt_d9L = np.zeros(4)
-        wgt_d10L2 = np.zeros(1)
+        wgt_d8_b1b1 = 0.0; wgt_d8_other = 0.0
+        wgt_b1L = 0.0; wgt_b1Lp = 0.0; wgt_d9L_other = 0.0
+        wgt_d10L2 = 0.0; wgt_d10L2p = 0.0
+        wgt_a1b1S1_Ls = 0.0; wgt_a1b1S0_Ls = 0.0
+        wgt_a1a1S1_Ls = 0.0; wgt_a1a1S0_Ls = 0.0
+        wgt_b1b1S1_Ls = 0.0; wgt_b1b1S0_Ls = 0.0
+        wgt_d8Ls_other = 0.0
+        wgt_a1L2s = 0.0; wgt_b1L2s = 0.0; wgt_d9L2s_other = 0.0
+        wgt_Lp = 0.0; wgt_ep = 0.0
 
         print ("Compute the weights in GS (lowest Aw peak)")
         #for i in indices[0]:
         for i in range(0,len(vecs[:,k])):
             # state is original state but its orbital info remains after basis change
             state = VS.get_state(VS.lookup_tbl[i])
+            
+            weight = abs(vecs[i,k])**2
             
             if state['type'] == 'two_hole_no_eh':
                 s1 = state['hole1_spin']
@@ -84,9 +92,31 @@ def get_ground_state(matrix, VS, S_val,Sz_val):
                 o12 = tuple(o12)
             
                 if i in indices[0]:
-                    print ('no e-h state ', s1,orb1,x1,y1,z1,s2,orb2,x2,y2,z2, 'S=',S12,'Sz=',Sz12, \
-                      ", weight = ", abs(vecs[i,k])**2)
+                    if not(abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1.):
+                        print ('no e-h state ', i, s1,orb1,x1,y1,s2,orb2,x2,y2, 'S=',S12,'Sz=',Sz12, \
+                                ", weight = ", weight)
                         
+                if orb1 in pam.Ni_orbs and orb2 in pam.Ni_orbs:
+                    if o12==('dx2y2','dx2y2'):
+                        wgt_d8_b1b1 += weight
+                    else:
+                        wgt_d8_other += weight
+
+                elif o12[0] in pam.Ni_orbs and o12[1] in pam.O_orbs:
+                    if o12[0]=='dx2y2':
+                        if abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1.:
+                            wgt_b1Lp += weight
+                        else:
+                            wgt_b1L  += weight
+                    else:
+                        wgt_d9L_other += weight
+
+                elif o12[0] in pam.O_orbs and o12[1] in pam.O_orbs:
+                    if abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1.:
+                        wgt_d10L2p += weight
+                    else:
+                        wgt_d10L2  += weight
+
             if state['type'] == 'two_hole_one_eh':
                 se = state['e_spin']
                 s1 = state['hole1_spin']
@@ -100,35 +130,92 @@ def get_ground_state(matrix, VS, S_val,Sz_val):
                 x1, y1, z1 = state['hole1_coord']
                 x2, y2, z2 = state['hole2_coord']
                 x3, y3, z3 = state['hole3_coord']
-
+                
+                S12  = S_val[i]
+                Sz12 = Sz_val[i]
+                
+                o12 = sorted([orb1,orb2])
+                o12 = tuple(o12)
+                o23 = sorted([orb2,orb3])
+                o23 = tuple(o23)
+                o13 = sorted([orb1,orb3])
+                o13 = tuple(o13)
+                
+                nNi, nO, dorbs, porbs = util.get_statistic_3orb(orb1,orb2,orb3)
+                
                 #if abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1.:
                 #    continue
+                
                 if i in indices[0]:
-                    print ('one e-h state ', se,orbe,xe,ye,s1,orb1,x1,y1,s2,orb2,x2,y2,s3,orb3,x3,y3, \
-                          ", weight = ", abs(vecs[i,k])**2)
+                    if not(abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1. or abs(x3)>1. or abs(y3)>1.):
+                        print ('one e-h state ', i, se,orbe,xe,ye,ze,s1,orb1,x1,y1,s2,orb2,x2,y2,s3,orb3,x3,y3, \
+                               'S=',S12,'Sz=',Sz12, ", weight = ", weight)
 
-                # record the weights of 1A1 and 3B1 states a1a1, b1b1, ..., a1b1 in G.S.
-#                 if o12[0]==o12[1]=='d3z2r2':
-#                     wgt_d8[0] += abs(vecs[i,k])**2
-#                 if o12==('dx2y2','dx2y2'):
-#                     wgt_d8[1] += abs(vecs[i,k])**2
-#                 if o12[0]==o12[1]=='dxy':
-#                     wgt_d8[2] += abs(vecs[i,k])**2
-#                 if o12[0]=='d3z2r2' and o12[1]=='dx2y2':
-#                     wgt_d8[4] += abs(vecs[i,k])**2
-#                 if o12[0]=='d3z2r2' and o12[1]=='dxy':
-#                     wgt_d8[5] += abs(vecs[i,k])**2
+                if not(abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1. or abs(x3)>1. or abs(y3)>1.\
+                       or abs(xe)>0. or abs(ye)>0.):
+                    # d8Ls
+                    if nNi==2:
+                        if o12==('d3z2r2','dx2y2') or o23==('d3z2r2','dx2y2') or o13==('d3z2r2','dx2y2'):
+                            if S12==1:
+                                wgt_a1b1S1_Ls += weight
+                            elif S12==0:
+                                wgt_a1b1S0_Ls += weight
+                        elif o12==('dx2y2','dx2y2') or o23==('dx2y2','dx2y2') or o13==('dx2y2','dx2y2'):
+                            if S12==1:
+                                wgt_b1b1S1_Ls += weight
+                            elif S12==0:
+                                wgt_b1b1S0_Ls += weight
+                        elif o12==('d3z2r2','d3z2r2') or o23==('d3z2r2','d3z2r2') or o13==('d3z2r2','d3z2r2'):
+                            if S12==1:
+                                wgt_a1a1S1_Ls += weight
+                            elif S12==0:
+                                wgt_a1a1S0_Ls += weight
+                        else:
+                            wgt_d8Ls_other += weight
 
-#                 if o12[0]=='d3z2r2' and o12[1] in pam.O_orbs:
-#                     wgt_d9L[0] += abs(vecs[i,k])**2
-#                 if o12[0]=='dx2y2'  and o12[1] in pam.O_orbs:
-#                     wgt_d9L[1] += abs(vecs[i,k])**2
-#                 if o12[0]=='dxy'    and o12[1] in pam.O_orbs:
-#                     wgt_d9L[2] += abs(vecs[i,k])**2
-#                 if (o12[0]=='dxz' or o12[0]=='dyz') and o12[1] in pam.O_orbs:
-#                     wgt_d9L[3] += abs(vecs[i,k])**2
+                    elif nNi==1:
+                        if dorbs[0]=='d3z2r2':
+                            wgt_a1L2s += weight
+                        elif dorbs[0]=='dx2y2':
+                            wgt_b1L2s += weight
+                        else:
+                            wgt_d9L2s_other += weight
 
-#                 if o12[0] in pam.O_orbs and o12[1] in pam.O_orbs:
-#                     wgt_d10L2[0] += abs(vecs[i,k])**2
-
-    return vals, vecs, wgt_d8, wgt_d9L, wgt_d10L2
+                elif (xe,ye)==(0,0) and (abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1. or abs(x3)>1. or abs(y3)>1.):
+                    wgt_Lp += weight
+                        
+                elif (xe,ye)!=(0,0) and \
+                    not (abs(x1)>1. or abs(y1)>1. or abs(x2)>1. or abs(y2)>1. or abs(x3)>1. or abs(y3)>1.):
+                    wgt_ep += weight
+                
+    print('wgt_d8_b1b1 = ',wgt_d8_b1b1)
+    print('wgt_d8_other = ',wgt_d8_other)
+    print('wgt_b1L = ',wgt_b1L)
+    print('wgt_b1Lp = ',wgt_b1Lp)
+    print('wgt_d9L_other = ',wgt_d9L_other)
+    print('wgt_d10L2 = ',wgt_d10L2)
+    print('wgt_d10L2p = ',wgt_d10L2p)
+    
+    print('wgt_a1b1S1_Ls = ',wgt_a1b1S1_Ls)
+    print('wgt_a1b1S0_Ls = ',wgt_a1b1S0_Ls)
+    print('wgt_a1a1S1_Ls = ',wgt_a1a1S1_Ls)
+    print('wgt_a1a1S0_Ls = ',wgt_a1a1S0_Ls)
+    print('wgt_b1b1S1_Ls = ',wgt_b1b1S1_Ls)
+    print('wgt_b1b1S0_Ls = ',wgt_b1b1S0_Ls)
+    print('wgt_d8Ls_other = ',wgt_d8Ls_other)
+    print('wgt_a1L2s = ',wgt_a1L2s)
+    print('wgt_b1L2s = ',wgt_b1L2s)
+    print('wgt_d9L2s_other = ',wgt_d9L2s_other)
+    
+    print('wgt_Lp = ',wgt_Lp)
+    print('wgt_ep = ',wgt_ep)
+    
+    print(wgt_d8_b1b1, wgt_d8_other, wgt_b1L, wgt_b1Lp, wgt_d9L_other, wgt_d10L2, wgt_d10L2p, \
+          wgt_a1b1S1_Ls, wgt_a1b1S0_Ls, wgt_a1a1S1_Ls, wgt_a1a1S0_Ls, wgt_b1b1S1_Ls, wgt_b1b1S0_Ls, wgt_d8Ls_other, \
+          wgt_a1L2s, wgt_b1L2s, wgt_d9L2s_other, wgt_Lp, wgt_ep)
+    
+    print('total weight = ', wgt_d8_b1b1+ wgt_d8_other+wgt_b1L+ wgt_b1Lp+ wgt_d9L_other+ wgt_d10L2+ wgt_d10L2p+ \
+          wgt_a1b1S1_Ls+ wgt_a1b1S0_Ls+ wgt_a1a1S1_Ls+ wgt_a1a1S0_Ls+ wgt_b1b1S1_Ls+ wgt_b1b1S0_Ls +\
+          wgt_d8Ls_other+ wgt_a1L2s+ wgt_b1L2s+wgt_d9L2s_other+ wgt_Lp+ wgt_ep)
+    
+    return vals #, vecs, wgt_d8, wgt_d9L, wgt_d10L2

d8_1eh_pair_impurity_model/parameters.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 M_PI = math.pi
 
-Mc = 2
+Mc = 5
 
 # Note that Ni-d and O-p orbitals use hole language
 # while Nd orbs use electron language
@@ -16,6 +16,11 @@ ed = {'d3z2r2': 1.2,\
       'dxy'   : 0.3,\
       'dxz'   : 0.7,\
       'dyz'   : 0.7}
+ed = {'d3z2r2': 1.2,\
+      'dx2y2' : 0.0,\
+      'dxy'   : 0.3,\
+      'dxz'   : 0.7,\
+      'dyz'   : 0.7}
 eps = np.arange(3.0, 3.01, 1.0)
 eOs = 2.0
 
@@ -23,9 +28,9 @@ As = np.arange(6.0, 6.01, 2.0)
 B = 0.15
 C = 0.58
 #As = np.arange(100, 100.1, 1.0)
-# As = np.arange(0.0, 0.01, 1.0)
-# B = 0
-# C = 0
+#As = np.arange(0.0, 0.01, 1.0)
+#B = 0
+#C = 0
 
 # Note: tpd and tpp are only amplitude signs are considered separately in hamiltonian.py
 # Slater Koster integrals and the overlaps between px and d_x^2-y^2 is sqrt(3) bigger than between px and d_3z^2-r^2 
@@ -33,8 +38,8 @@ C = 0.58
 
 # IMPORTANT: keep all hoppings below positive to avoid confusion
 #            hopping signs are considered in dispersion separately
-Norb = 12
-if Norb==4 or Norb==8:
+Norb = 10
+if Norb==8:
     #tpds = [0.00001]  # for check_CuO4_eigenvalues.py
     tpds = np.linspace(1.3, 1.3, num=1, endpoint=True) #[0.25]
     #tpds = [0.01]
@@ -77,7 +82,7 @@ eh_spin_def = 'same'  # 'oppo'
 basis_change_type = 'd_double' # 'all_states' or 'd_double'
 if_print_VS_after_basis_change = 0
 
-if_compute_Aw = 1
+if_compute_Aw = 0
 if if_compute_Aw==1:
     if_find_lowpeak = 0
     if if_find_lowpeak==1:
@@ -90,13 +95,13 @@ if if_compute_Aw==1:
 if_get_ground_state = 1
 if if_get_ground_state==1:
     # see issue https://github.com/scipy/scipy/issues/5612
-    Neval = 10
+    Neval = 3
 
 Ni_orbs = ['dx2y2','dxy','dxz','dyz','d3z2r2']
 #Ni_orbs = ['dx2y2','d3z2r2']
 Ovacancy_orbs = ['Os']
     
-if Norb==4 or Norb==8:
+if Norb==8:
     O1_orbs  = ['px']
     O2_orbs  = ['py']
 elif Norb==10:

d9_1eh_pair_impurity_model/basis_change.py
@@ -3,53 +3,19 @@ import lattice as lat
 import bisect
 import numpy as np
 import scipy.sparse as sps
-
-def count_VS(VS):
-    '''
-    Get statistics in VS
-    '''
-    count_upup = 0
-    count_updn = 0
-    count_dnup = 0
-    count_dndn = 0
-    
-    for i in xrange(0,VS.dim):
-        state = VS.get_state(VS.lookup_tbl[i])
-        
-        if state['type'] == 'one_hole_one_eh': 
-            s1 = state['hole1_spin']
-            s2 = state['hole2_spin']
-
-            if s1=='up' and s2=='up':
-                count_upup += 1
-            elif s1=='up' and s2=='dn':
-                count_updn += 1
-            elif s1=='dn' and s2=='up':
-                count_dnup += 1
-            elif s1=='dn' and s2=='dn':
-                count_dndn += 1
-
-    print 'No. of states with count_upup, count_updn, count_dnup, count_dndn:',\
-        count_upup, count_updn, count_dnup, count_dndn
-    
-    # only correct in most times, not mandatory
-    #assert(count_upup==count_dndn)
-    
-    return count_upup, count_updn, count_dnup, count_dndn
+import parameters as pam
             
 def find_singlet_triplet_partner(state,VS):
     '''
     For a given state find its partner state to form a singlet/triplet.
     Applies to general opposite-spin state, not nesessarily in d_double
+
     Returns
     -------
     index: index of the singlet/triplet partner state in the VS
     phase: phase factor with which the partner state needs to be multiplied.
     '''
-    if state['type'] == 'one_hole_one_eh': 
-        se = state['e_spin']
-        orbe = state['e_orb']
-        xe, ye, ze = state['e_coord']
+    if state['type'] == 'two_hole_no_eh': 
         s1 = state['hole1_spin']
         s2 = state['hole2_spin']
         orb1 = state['hole1_orb']
@@ -57,7 +23,8 @@ def find_singlet_triplet_partner(state,VS):
         x1, y1, z1 = state['hole1_coord']
         x2, y2, z2 = state['hole2_coord']
         
-        tmp_state = vs.create_one_hole_one_eh_state(se,orbe,xe,ye,ze,s2,orb1,x1,y1,z1,s1,orb2,x2,y2,z2)
+        slabel = [s2,orb1,x1,y1,z1,s1,orb2,x2,y2,z2]
+        tmp_state = vs.create_two_hole_no_eh_state(slabel)
         partner_state, phase = vs.make_state_canonical(tmp_state)
         
     return VS.get_index(partner_state), phase
@@ -67,12 +34,14 @@ def create_singlet_triplet_basis_change_matrix(VS,d_double):
     Create a matrix representing the basis change to singlets/triplets. The
     columns of the output matrix are the new basis vectors. 
     The Hamiltonian transforms as U_dagger*H*U. 
+
     Parameters
     ----------
     phase: dictionary containing the phase factors created with
         hamiltonian.create_phase_dict.
     VS: VariationalSpace class from the module variational_space. Should contain
         only zero-magnon states.
+
     Returns
     -------
     U: matrix representing the basis change to singlets/triplets in
@@ -99,7 +68,7 @@ def create_singlet_triplet_basis_change_matrix(VS,d_double):
     
     for i in range(0,VS.dim):
         start_state = VS.get_state(VS.lookup_tbl[i])
-        if start_state['type'] == 'one_hole_one_eh': 
+        if start_state['type'] == 'two_hole_no_eh': 
             s1 = start_state['hole1_spin']
             s2 = start_state['hole2_spin']
             orb1 = start_state['hole1_orb']
@@ -175,32 +144,32 @@ def create_singlet_triplet_basis_change_matrix(VS,d_double):
 
                     count_singlet += 1
                     count_triplet += 1
-
+                    
     return sps.coo_matrix((data,(row,col)),shape=(VS.dim,VS.dim))/np.sqrt(2.0), S_val, Sz_val, AorB_sym
                 
 def find_singlet_triplet_partner_d_double(state,VS):
     '''
     For a given state find its partner state to form a singlet/triplet.
     Right now only applied for d_double states
-
+    
     Returns
     -------
     index: index of the singlet/triplet partner state in the VS
     phase: phase factor with which the partner state needs to be multiplied.
     '''
-    if state['type'] == 'one_hole_one_eh': 
+    if state['type'] == 'one_hole_one_eh':
         se = state['e_spin']
-        orbe = state['e_orb']
-        xe, ye, ze = state['e_coord']
         s1 = state['hole1_spin']
         s2 = state['hole2_spin']
+        orbe = state['e_orb']
         orb1 = state['hole1_orb']
         orb2 = state['hole2_orb']
+        xe, ye, ze = state['e_coord']
         x1, y1, z1 = state['hole1_coord']
         x2, y2, z2 = state['hole2_coord']
         
-        tmp_state = vs.create_one_hole_one_eh_state(se,orbe,xe,ye,ze,s2,orb1,x1,y1,z1,s1,orb2,x2,y2,z2)
-        partner_state, phase = vs.make_state_canonical(tmp_state)
+        tmp_state = vs.create_one_hole_one_eh_state(se,orbe,xe,ye,ze,'up',orb2,x2,y2,z2,'dn',orb1,x1,y1,z1)
+        partner_state,phase = vs.make_state_canonical(tmp_state)
         
     return VS.get_index(partner_state), phase
 
@@ -208,6 +177,9 @@ def create_singlet_triplet_basis_change_matrix_d_double(VS,d_double):
     '''
     Similar to above create_singlet_triplet_basis_change_matrix but only applies
     basis change for d_double states
+    
+    Note that for three hole state, its partner state must have exactly the same
+    spin and positions of L and Nd-electron
     '''
     data = []
     row = []
@@ -231,17 +203,23 @@ def create_singlet_triplet_basis_change_matrix_d_double(VS,d_double):
             data.append(np.sqrt(2.0)); row.append(i); col.append(i)
         
     for i in d_double:
-        # d_double must be one_eh state
         start_state = VS.get_state(VS.lookup_tbl[i])
+        itype = start_state['type']
+        assert(itype == 'one_hole_one_eh')
+        
         se = start_state['e_spin']
         s1 = start_state['hole1_spin']
         s2 = start_state['hole2_spin']
         oe = start_state['e_orb']
-        orb1 = start_state['hole1_orb']
-        orb2 = start_state['hole2_orb']
+        o1 = start_state['hole1_orb']
+        o2 = start_state['hole2_orb']
         xe, ye, ze = start_state['e_coord']
+        x1, y1, z1 = start_state['hole1_coord']
+        x2, y2, z2 = start_state['hole2_coord']
+
         epos=[xe, ye, ze]
 
+        # note the following is generic for two types of states
         if s1==s2:
             # must be triplet
             # see case 2 of make_state_canonical in vs.py, namely
@@ -253,41 +231,55 @@ def create_singlet_triplet_basis_change_matrix_d_double(VS,d_double):
             elif s1=='dn':
                 Sz_val[i] = -1
             count_triplet += 1
-            
+
         elif s1=='dn' and s2=='up':
-            print 'Error: d_double cannot have states with s1=dn, s2=up !'
+            print ('Error: d_double cannot have states with s1=dn, s2=up !')
+            tstate = VS.get_state(VS.lookup_tbl[i])
+            tse = tstate['e_spin']
+            ts1 = tstate['hole1_spin']
+            ts2 = tstate['hole2_spin']
+            torbe = tstate['e_orb']
+            torb1 = tstate['hole1_orb']
+            torb2 = tstate['hole2_orb']
+            txe, tye, tze = tstate['e_coord']
+            tx1, ty1, tz1 = tstate['hole1_coord']
+            tx2, ty2, tz2 = tstate['hole2_coord']
+            print ('Error state', i, tse,torbe,txe,tye,ts1,torb1,tx1,ty1,ts2,torb2,tx2,ty2)
             break
-            
+
         elif s1=='up' and s2=='dn':
-            if orb1==orb2:               
-                # get state as (e1e1 +- e2e2)/sqrt(2) for A and B sym separately 
-                # instead of e1e1 and e2e2
-                if orb1!='dxz' and orb1!='dyz':
+            if o1==o2: 
+                if o1!='dxz' and o1!='dyz':
                     data.append(np.sqrt(2.0));  row.append(i); col.append(i)
                     S_val[i]  = 0
                     Sz_val[i] = 0
                     count_singlet += 1
-                elif orb1=='dxz':  # no need to consider e2='dyz' case
+                    
+                # get state as (e1e1 +- e2e2)/sqrt(2) for A and B sym separately 
+                # instead of e1e1 and e2e2
+                elif o1=='dxz':  # no need to consider e2='dyz' case
                     # find e2e2 state:
                     for e2 in d_double:
                         state = VS.get_state(VS.lookup_tbl[e2])
+                        jtype = state['type']
+                        assert(jtype == 'one_hole_one_eh')
+                        
                         jse = state['e_spin']
                         js1 = state['hole1_spin']
                         js2 = state['hole2_spin']
                         joe = state['e_orb']
-                        jorb1 = state['hole1_orb']
-                        jorb2 = state['hole2_orb']
+                        jo1 = state['hole1_orb']
+                        jo2 = state['hole2_orb']
                         jxe, jye, jze = state['e_coord']
-                        
+                        jx1, jy1, jz1 = state['hole1_coord']
+                        jx2, jy2, jz2 = state['hole2_coord']
+
                         jepos=[jxe, jye, jze]
-                        
-                        # the other Nd-e should have exactly the same s, orb, position
-                        # this is similar to two hole case, which is more complicated 
-                        # because of another additional L hole
-                        if not jse==se and joe==oe and jepos==epos:
+
+                        if not (jse==se and joe==oe and jepos==epos):
                             continue
-        
-                        if jorb1==jorb2=='dyz':
+                           
+                        if jo1==jo2=='dyz':
                             data.append(1.0);  row.append(i);  col.append(i)
                             data.append(1.0);  row.append(e2); col.append(i)
                             AorB_sym[i]  = 1
@@ -300,16 +292,17 @@ def create_singlet_triplet_basis_change_matrix_d_double(VS,d_double):
                             S_val[e2]  = 0
                             Sz_val[e2] = 0
                             count_singlet += 1
-                                
+
             else:
                 if i not in count_list:
                     j, ph = find_singlet_triplet_partner_d_double(start_state,VS)
+                    assert(j!=i)
 
                     # append matrix elements for singlet states
                     # convention: original state col i stores singlet and 
                     #             partner state col j stores triplet
                     data.append(1.0);  row.append(i); col.append(i)
-                    data.append(-ph);  row.append(j); col.append(i)
+                    data.append(ph);  row.append(j); col.append(i)
                     S_val[i]  = 0
                     Sz_val[i] = 0
 
@@ -319,7 +312,7 @@ def create_singlet_triplet_basis_change_matrix_d_double(VS,d_double):
 
                     # append matrix elements for triplet states
                     data.append(1.0);  row.append(i); col.append(j)
-                    data.append(ph);   row.append(j); col.append(j)
+                    data.append(-ph);   row.append(j); col.append(j)
                     S_val[j]  = 1
                     Sz_val[j] = 0
 
@@ -330,17 +323,21 @@ def create_singlet_triplet_basis_change_matrix_d_double(VS,d_double):
 
     return sps.coo_matrix((data,(row,col)),shape=(VS.dim,VS.dim))/np.sqrt(2.0), S_val, Sz_val, AorB_sym
 
-def print_VS_after_basis_change(VS,S_val,Sz_val):
-    print 'print_VS_after_basis_change:'
-    for i in xrange(0,VS.dim):
+def print_VS_after_basis_change(VS,d_double,S_val,Sz_val):
+    print ('print_VS_after_basis_change for d_double states:')
+    for i in d_double:
         state = VS.get_state(VS.lookup_tbl[i])
+        assert(state['type'] == 'one_hole_one_eh')
+        
+        se = state['e_spin']
+        orbe = state['e_orb']
+        xe, ye, ze = state['e_coord']
         ts1 = state['hole1_spin']
         ts2 = state['hole2_spin']
         torb1 = state['hole1_orb']
         torb2 = state['hole2_orb']
         tx1, ty1, tz1 = state['hole1_coord']
         tx2, ty2, tz2 = state['hole2_coord']
-        #if ts1=='up' and ts2=='up':
-        if torb1=='dx2y2' and torb2=='px':
-            print i, ts1,torb1,tx1,ty1,tz1,ts2,torb2,tx2,ty2,tz2,'S=',S_val[i],'Sz=',Sz_val[i]
+        
+        print (i, se,orbe,xe,ye,ze,ts1,torb1,tx1,ty1,tz1,ts2,torb2,tx2,ty2,tz2,'S=',S_val[i],'Sz=',Sz_val[i])
             

d9_1eh_pair_impurity_model/compute_Aw_1hole.py
@@ -20,18 +20,15 @@ import basis_change as basis
 import get_state as getstate
 import utility as util
 import plotfig as fig
-import ground_state_eigsh as gs
-#import ground_state_lanczos as gs
+import ground_state as gs
 import lanczos
 import time
 start_time = time.time()
 M_PI = math.pi
-                  
+                    
 #####################################
 def compute_Aw_main(A,ep,tpd,tpp,pds,pdp,pps,ppp,tNiOs,tOsOs,tOsOs_p,Upp,\
-                    d_double_no_eh, p_double_no_eh, \
-                    d_double_one_eh, p_double_one_eh, \
-                    e_part, double_part, idx, hole3_part, U, S_val, Sz_val, AorB_sym):
+                    d_double,p_double,U, S_val, Sz_val, AorB_sym): 
     if Norb==8:
         fname = 'ep'+str(ep)+'_eOs'+str(pam.eOs)+'_tpd'+str(tpd)+'_tpp'+str(tpp) \
                   +'_tNiOs'+str(tNiOs)+'_tOsOs'+str(tOsOs)+'_Mc'+str(Mc)+'_Norb'+str(Norb)+'_eta'+str(eta)
@@ -40,11 +37,6 @@ def compute_Aw_main(A,ep,tpd,tpp,pds,pdp,pps,ppp,tNiOs,tOsOs,tOsOs_p,Upp,\
         fname = 'ep'+str(ep)+'_eOs'+str(pam.eOs)+'_pdp'+str(pdp)+'_pps'+str(pps)+'_ppp'+str(ppp) \
                   +'_tNiOs'+str(tNiOs)+'_tOsOs'+str(tOsOs)+'_Mc'+str(Mc)+'_Norb'+str(Norb)+'_eta'+str(eta)
         flowpeak = 'Norb'+str(Norb)+'_pps'+str(pps)+'_ppp'+str(ppp)+'_Mc'+str(Mc)+'_eta'+str(eta)
-                
-    w_vals = np.arange(pam.wmin, pam.wmax, pam.eta/4.0)
-    Aw = np.zeros(len(w_vals))
-    Aw_dd_total = np.zeros(len(w_vals))
-    Aw_d8_total = np.zeros(len(w_vals))
 
     # set up H0
     if Norb==8:
@@ -58,8 +50,10 @@ def compute_Aw_main(A,ep,tpd,tpp,pds,pdp,pps,ppp,tNiOs,tOsOs,tOsOs_p,Upp,\
             
     T_pd   = ham.create_tpd_nn_matrix(VS,tpd_nn_hop_dir, tpd_orbs, tpd_nn_hop_fac)
     T_pp   = ham.create_tpp_nn_matrix(VS,tpp_nn_hop_fac)  
-    T_NiOs = ham.create_tNiOs_nn_matrix_from_no_eh \
-                (VS,tNiOs_nn_hop_dir, tNiOs_orbs, tNiOs_nn_hop_fac)
+    if pam.set_tNiOs_from=='no-eh':
+        T_NiOs = ham.create_tNiOs_nn_matrix_from_no_eh (VS,tNiOs_nn_hop_dir, tNiOs_orbs, tNiOs_nn_hop_fac)
+    elif pam.set_tNiOs_from=='one-eh':
+        T_NiOs = ham.create_tNiOs_nn_matrix_from_one_eh(VS,tNiOs_nn_hop_dir, tNiOs_orbs, tNiOs_nn_hop_fac)
     T_Os   = ham.create_tOsOs_nn_matrix(VS,tOsOs,tOsOs_p)
     Esite  = ham.create_edepeOs_diag_matrix(VS,A,ep)
     
@@ -76,58 +70,54 @@ def compute_Aw_main(A,ep,tpd,tpp,pds,pdp,pps,ppp,tNiOs,tOsOs,tOsOs_p,Upp,\
     plt.clf()
 
     if Norb==8 or Norb==10 or Norb==11 or Norb==12:     
-        Hint_no_eh  = ham.create_interaction_matrix_ALL_syms_no_eh(VS,d_double_no_eh,p_double_no_eh, \
-                                                                   S_val, Sz_val,AorB_sym,A,Upp)
-        Hint_one_eh = ham.create_interaction_matrix_ALL_syms_one_eh(VS,d_double_one_eh, p_double_one_eh,\
-                          e_part, double_part, idx, hole3_part, S_val, Sz_val, AorB_sym, A, Upp)
-        if pam.if_H0_rotate_byU==1:
-            H = H0_new + Hint_no_eh + Hint_one_eh 
-        else:
-            H = H0 + Hint_no_eh + Hint_one_eh  
-        H.tocsr()
+        Hint = ham.create_interaction_matrix_ALL_syms(VS,d_double,p_double,S_val, Sz_val,AorB_sym,A,Upp)
 
+        if pam.if_H0_rotate_byU==1:
+            H = H0_new + Hint 
+        else:
+            H = H0 + Hint 
+        H.tocsr()
+        
         ####################################################################################
         # compute GS only for turning on full interactions
         if pam.if_get_ground_state==1:
             vals = gs.get_ground_state(H, VS, S_val,Sz_val)
-#            if Norb==8:
-#                util.write_GS('Egs_'+flowpeak+'.txt',A,ep,tpd,vals[0])
-#                util.write_GS_components('GS_weights_'+flowpeak+'.txt',A,ep,tpd,wgt_d8, wgt_d9L, wgt_d10L2)
-#            elif Norb==10 or Norb==11 or Norb==12:
-#                util.write_GS2('Egs_'+flowpeak+'.txt',A,ep,pds,pdp,vals[0])
-#                util.write_GS_components2('GS_weights_'+flowpeak+'.txt',A,ep,pds,pdp,wgt_d8, wgt_d9L, wgt_d10L2)
+            if Norb==8:
+                util.write_GS('Egs_'+flowpeak+'.txt',A,ep,tpd,vals[0])
+               # util.write_GS_components('GS_weights_'+flowpeak+'.txt',A,ep,tpd,wgt_d8, wgt_d9L, wgt_d10L2)
+            elif Norb==10 or Norb==11 or Norb==12:
+                util.write_GS2('Egs_'+flowpeak+'.txt',A,ep,pds,pdp,vals[0])
+               # util.write_GS_components2('GS_weights_'+flowpeak+'.txt',A,ep,pds,pdp,wgt_d8, wgt_d9L, wgt_d10L2)
             
-        #########################################################################
+        ####################################################################################
         '''
         Compute A(w) for various states
         '''
         if pam.if_compute_Aw==1:
-            # compute d8
-            fig.compute_Aw_d8_sym(H, VS, d_double_no_eh, S_val, Sz_val, AorB_sym, A, w_vals, "Aw_d8_sym_", fname)
+            w_vals = np.arange(pam.wmin, pam.wmax, pam.eta/4.0)
 
-            # compute d9L
-#             b1L_state_indices, a1L_state_indices, b1L_state_labels, a1L_state_labels \
-#                     = getstate.get_d9L_state_indices(VS, S_val, Sz_val)
-#             fig.compute_Aw1(H, VS, w_vals, b1L_state_indices, b1L_state_labels, "Aw_b1L_", fname)
-#             fig.compute_Aw1(H, VS, w_vals, a1L_state_indices, a1L_state_labels, "Aw_a1L_", fname)
+            # compute d9
+            d9_state_indices, d9_state_labels, d9_labels = getstate.get_d9_state_indices(VS)
+            fig.compute_Aw(H, VS, w_vals, d9_state_indices, d9_labels, "Aw_d9_", fname)
 
-#             # compute d10L2
-#             d10L2_state_indices, d10L2_state_labels = getstate.get_d10L2_state_indices(VS, S_val, Sz_val)
-#             fig.compute_Aw1(H, VS, w_vals, d10L2_state_indices, d10L2_state_labels, "Aw_d10L2_", fname)
+            # compute d10L
+            d10L_state_indices, d10L_state_labels = getstate.get_d10L_state_indices(VS)
+            fig.compute_Aw(H, VS, w_vals, d10L_state_indices, d10L_state_labels, "Aw_d10L_", fname)
 
-#             # compute d8Ls for some special states
-#             a1b1Ls_S0_state_indices, a1b1Ls_S0_state_labels, \
-#             a1b1Ls_S1_state_indices, a1b1Ls_S1_state_labels, \
-#             a1a1Ls_state_indices, a1a1Ls_state_labels \
-#                                             = getstate.get_d8Ls_state_indices(VS, d_double_one_eh, S_val, Sz_val)
-#             fig.compute_Aw1(H, VS, w_vals, a1b1Ls_S0_state_indices, a1b1Ls_S0_state_labels, "Aw_a1b1Ls_S0_", fname)
-#             fig.compute_Aw1(H, VS, w_vals, a1b1Ls_S1_state_indices, a1b1Ls_S1_state_labels, "Aw_a1b1Ls_S1_", fname)
-#             fig.compute_Aw1(H, VS, w_vals, a1a1Ls_state_indices, a1a1Ls_state_labels, "Aw_a1a1Ls_", fname)
+            # compute d8s for some special states
+            d8s_state_indices, d8s_state_labels = getstate.get_d8s_state_indices(VS, d_double, \
+                                                                                 S_val, Sz_val, AorB_sym, A)
+            fig.compute_Aw(H, VS, w_vals, d8s_state_indices, d8s_state_labels, "Aw_d8s_", fname)
 
-#             # compute d9L2s
-#             d9L2s_state_indices, d9L2s_state_labels = getstate.get_d9L2s_state_indices(VS)
-#             fig.compute_Aw1(H, VS, w_vals, d9L2s_state_indices, d9L2s_state_labels, "Aw_d9L2s_", fname)
-        
+            # compute d8s according to d8's sym
+            fig.compute_Aw_d8s_sym(H, VS, d_double, S_val, Sz_val, AorB_sym, A, w_vals, "Aw_d8s_sym_", fname)
+
+            # compute d9Ls
+            b1Ls_state_indices, b1Ls_state_labels, a1Ls_state_indices, a1Ls_state_labels \
+                        = getstate.get_d9Ls_state_indices(VS, S_val, Sz_val)
+            fig.compute_Aw1(H, VS, w_vals, b1Ls_state_indices, b1Ls_state_labels, "Aw_b1Ls_", fname)
+            fig.compute_Aw1(H, VS, w_vals, a1Ls_state_indices, a1Ls_state_labels, "Aw_a1Ls_", fname)
+                              
     
 ##########################################################################
 if __name__ == '__main__': 
@@ -147,23 +137,21 @@ if __name__ == '__main__':
     
     # set up VS
     VS = vs.VariationalSpace(Mc)
-    
-    d_double_no_eh, p_double_no_eh, \
-    d_double_one_eh, p_double_one_eh, e_part, double_part, idx, hole3_part = ham.get_double_occu_list(VS)
+    d_double, p_double = ham.get_double_occu_list(VS)
     
     # change the basis for d_double states to be singlet/triplet
     if pam.basis_change_type=='all_states':
-        U, S_val, Sz_val, AorB_sym = basis.create_singlet_triplet_basis_change_matrix(VS, \
-                                d_double_no_eh, d_double_one_eh, e_part, double_part, idx, hole3_part)
-        if pam.if_print_VS_after_basis_change==1:
-            basis.print_VS_after_basis_change(VS,S_val,Sz_val)
+        U, S_val, Sz_val, AorB_sym = basis.create_singlet_triplet_basis_change_matrix(VS,d_double)
     elif pam.basis_change_type=='d_double':
-        U, S_val, Sz_val, AorB_sym = basis.create_singlet_triplet_basis_change_matrix_d_double(VS, \
-                                d_double_no_eh, d_double_one_eh, e_part, double_part, idx, hole3_part)
+        U, S_val, Sz_val, AorB_sym = basis.create_singlet_triplet_basis_change_matrix_d_double(VS,d_double)
+        
+    if pam.if_print_VS_after_basis_change==1:
+        basis.print_VS_after_basis_change(VS,d_double,S_val,Sz_val)
 
     U_d = (U.conjugate()).transpose()
+    
     # check if U if unitary
-    #checkU_unitary(U,U_d)
+    #util.checkU_unitary(U,U_d)
     
     if Norb==8:
         for tpd in pam.tpds:
@@ -174,12 +162,9 @@ if __name__ == '__main__':
                         for Upp in pam.Upps:
                             print ('===================================================')
                             print ('A=',A, 'ep=', ep, ' tpd=',tpd,' tpp=',tpp,\
-                                  ' tNiOs=',tNiOs, ' tOsOs=',tOsOs,' Upp=',Upp) 
+                                  ' tNiOs=',tNiOs, ' tOsOs=',tOsOs,' Upp=',Upp)
                             compute_Aw_main(A,ep,tpd,tpp,0,0,0,0,tNiOs,tOsOs,tOsOs_p,Upp,\
-                                            d_double_no_eh, p_double_no_eh, \
-                                            d_double_one_eh, p_double_one_eh, \
-                                            e_part, double_part, idx, hole3_part, \
-                                            U, S_val, Sz_val, AorB_sym)
+                                            d_double,p_double,U, S_val, Sz_val, AorB_sym)
     elif Norb==10 or Norb==11 or Norb==12:
         pps = pam.pps
         ppp = pam.ppp
@@ -194,9 +179,6 @@ if __name__ == '__main__':
                         print ('A=',A, 'ep=',ep,' pds=',pds,' pdp=',pdp,' pps=',pps,' ppp=',ppp,\
                               ' tNiOs=',tNiOs, ' tOsOs=',tOsOs,' Upp=',Upp)
                         compute_Aw_main(A,ep,0,0,pds,pdp,pps,ppp,tNiOs,tOsOs,tOsOs_p,Upp,\
-                                        d_double_no_eh, p_double_no_eh, \
-                                        d_double_one_eh, p_double_one_eh, \
-                                        e_part, double_part, idx, hole3_part, \
-                                        U, S_val, Sz_val, AorB_sym)      
+                                        d_double,p_double,U, S_val, Sz_val, AorB_sym)      
                         
     print("--- %s seconds ---" % (time.time() - start_time))

d9_1eh_pair_impurity_model/parameters.py
@@ -2,24 +2,29 @@ import math
 import numpy as np
 M_PI = math.pi
 
-Mc = 3
+Mc = 10
 
 # Note that Ni-d and O-p orbitals use hole language
 # while Nd orbs use electron language
-ed = {'d3z2r2': -0.45,\
-      'dx2y2' : -1.77,\
-      'dxy'   : -0.4,\
-      'dxz'   : -0.35,\
-      'dyz'   : -0.35}
-ed = {'d3z2r2': 0,\
-      'dx2y2' : 0,\
-      'dxy'   : 0,\
-      'dxz'   : 0,\
-      'dyz'   : 0}
-eNd = 4.0
+ed = {'d3z2r2': 0.0,\
+      'dx2y2' : 0.0,\
+      'dxy'   : 0.0,\
+      'dxz'   : 0.0,\
+      'dyz'   : 0.0}
+ed = {'d3z2r2': -0.1,\
+      'dx2y2' : 0.0,\
+      'dxy'   : -0.1,\
+      'dxz'   : 0.0,\
+      'dyz'   : 0.0}
+ed = {'d3z2r2': 1.2,\
+      'dx2y2' : 0.0,\
+      'dxy'   : 0.3,\
+      'dxz'   : 0.7,\
+      'dyz'   : 0.7}
+eps = np.arange(3.0, 3.01, 1.0)
+eOs = 2.0
 
-eps = np.arange(7.0, 7.01, 1.0) #[3.5]#,3.5,4.5]
-As = np.arange(6.0, 6.01, 1.0)
+As = np.arange(6.0, 6.01, 2.0)
 B = 0.15
 C = 0.58
 #As = np.arange(100, 100.1, 1.0)
@@ -28,31 +33,36 @@ C = 0.58
 #C = 0
 
 # Note: tpd and tpp are only amplitude signs are considered separately in hamiltonian.py
-# Slater Koster integrals and the overlaps between px and d_x^2-y^2 is sqrt(3) bigger than between px and d_3x^2-r^2 
+# Slater Koster integrals and the overlaps between px and d_x^2-y^2 is sqrt(3) bigger than between px and d_3z^2-r^2 
 # These two are proportional to tpd,sigma, whereas the hopping between py and d_xy is proportional to tpd,pi
 
 # IMPORTANT: keep all hoppings below positive to avoid confusion
 #            hopping signs are considered in dispersion separately
 Norb = 12
-if Norb==3 or Norb==8:
+if Norb==8:
     #tpds = [0.00001]  # for check_CuO4_eigenvalues.py
-    tpds = np.linspace(1.5, 1.5, num=1, endpoint=True) #[0.25]
-    #tpds = [2.001]
+    tpds = np.linspace(1.3, 1.3, num=1, endpoint=True) #[0.25]
+    #tpds = [0.01]
     tpps = [0.55]
-    tNiNds = [0.5]
-    tNdNds = [0.2]
-elif Norb==10 or Norb==11 or Norb==12:    
+    #tpps = [0.001]
+    tNiOs = 1.13
+    #tNiOs = 0.01 
+    tOsOs = 0.23
+    tOsOs_p = 0.44  # hopping between two planes
+elif Norb==10 or Norb==12:    
     # pdp = sqrt(3)/4*pds so that tpd(b2)=tpd(b1)/2: see Eskes's thesis and 1990 paper
     # the values of pds and pdp between papers have factor of 2 difference
     # here use Eskes's thesis Page 4
     # also note that tpd ~ pds*sqrt(3)/2
-    vals = np.linspace(1.5, 1.5, num=1, endpoint=True)
+    vals = np.linspace(1.3, 1.3, num=1, endpoint=True)
     pdss = np.asarray(vals)*2./np.sqrt(3)
     pdps = np.asarray(pdss)*np.sqrt(3)/4.
-    #pdss = [1.5]
-    #pdps = [0.7]
-    tNiNds = [0.5]
-    tNdNds = [0.2]
+    #pdss = [0.01]
+    #pdps = [0.01]
+    tNiOs = 1.13
+    #tNiOs = 0.01
+    tOsOs = 0.23
+    tOsOs_p = 0.44  # hopping between two planes
     #------------------------------------------------------------------------------
     # note that tpp ~ (pps+ppp)/2
     # because 3 or 7 orbital bandwidth is 8*tpp while 9 orbital has 4*(pps+ppp)
@@ -61,81 +71,67 @@ elif Norb==10 or Norb==11 or Norb==12:
     #pps = 0.00001
     #ppp = 0.00001
 
-eta = 0.02
+wmin = -8; wmax = 20
+eta = 0.4
 Lanczos_maxiter = 600
 
 # restriction on variational space
 reduce_VS = 1
+if_H0_rotate_byU = 1
 
+# set Ni-Os hopping from no_eh states or one_eh states:
+set_tNiOs_from = 'no-eh'  # 'one-eh'
+
+eh_spin_def = 'same'  # 'oppo'
 basis_change_type = 'd_double' # 'all_states' or 'd_double'
+if_print_VS = 0
 if_print_VS_after_basis_change = 0
 
-if_find_lowpeak = 0
-if if_find_lowpeak==1:
-    peak_mode = 'lowest_peak' # 'lowest_peak' or 'highest_peak' or 'lowest_peak_intensity'
-    if_write_lowpeak_ep_tpd = 1
-if_write_Aw = 0
-if_savefig_Aw = 0
+if_compute_Aw = 0
+if if_compute_Aw==1:
+    if_find_lowpeak = 0
+    if if_find_lowpeak==1:
+        peak_mode = 'highest_peak' # 'lowest_peak' or 'highest_peak' or 'lowest_peak_intensity'
+        if_write_lowpeak_ep_tpd = 1
+    if_write_Aw = 1
+    if_savefig_Aw = 1
+    if_compute_Aw_dd_total = 0
 
 if_get_ground_state = 1
 if if_get_ground_state==1:
     # see issue https://github.com/scipy/scipy/issues/5612
-    Neval = 10
-if_compute_Aw_dd_total = 0
-if_compute_Aw_pp = 0
-if_compute_Aw_dp = 0
-if_compute_Aw_Cu_dx2y2_O = 0
+    Neval = 20
 
-if Norb==3:
-    Ni_orbs = ['dx2y2']
-else:
-    Ni_orbs = ['dx2y2','dxy','dxz','dyz','d3z2r2']
-    #Ni_orbs = ['dx2y2','d3z2r2']
-    Nd_orbs = ['Nd_s']
+Ni_orbs = ['dx2y2','dxy','dxz','dyz','d3z2r2']
+#Ni_orbs = ['dx2y2','d3z2r2']
+Ovacancy_orbs = ['Os']
     
-if Norb==3 or Norb==8:
+if Norb==8:
     O1_orbs  = ['px']
     O2_orbs  = ['py']
-    ap_orbs  = []
 elif Norb==10:
     O1_orbs  = ['px1','py1']
     O2_orbs  = ['px2','py2']
-    ap_orbs  = []
 elif Norb==11:
     O1_orbs  = ['px1','py1']
     O2_orbs  = ['px2','py2']
-    ap_orbs  = ['apz']
 elif Norb==12:
     O1_orbs  = ['px1','py1','pz1']
     O2_orbs  = ['px2','py2','pz2']
-    ap_orbs  = []
-O_orbs = O1_orbs + O2_orbs + ap_orbs
+O_orbs = O1_orbs + O2_orbs
 # sort the list to facilliate the setup of interaction matrix elements
 Ni_orbs.sort()
-Nd_orbs.sort()
 O1_orbs.sort()
 O2_orbs.sort()
-ap_orbs.sort()
 O_orbs.sort()
-print "Ni_orbs = ", Ni_orbs
-print "Nd_orbs = ", Nd_orbs
-print "O1_orbs = ",  O1_orbs
-print "O2_orbs = ",  O2_orbs
-print "ap_orbs = ",  ap_orbs
-orbs = Ni_orbs + Nd_orbs + O_orbs 
+print ("Ni_orbs = ", Ni_orbs)
+print ("O1_orbs = ", O1_orbs)
+print ("O2_orbs = ", O2_orbs)
+print ("Ovacancy_orbs = ",  Ovacancy_orbs)
+orbs = Ni_orbs + O_orbs + Ovacancy_orbs
 #assert(len(orbs)==Norb)
-# ======================================================================
-# Below for interaction matrix
+
 Upps = [0]
-if Norb==3:
-    Udds = As+4*B+3*C
-    #Udds = [100.]
-else:
-    interaction_sym = ['ALL']#,'3B1']#,'1A2','3A2','1B1','3B1','1E','3E']#,'1B2','3B2']
-    print "turn on interactions for symmetries = ",interaction_sym
-    
-    if interaction_sym == ['ALL']:
-        symmetries = ['1A1','1B1','3B1','1A2','3A2','1E','3E']
-    else:
-        symmetries = ['1A1']
-    print "compute A(w) for symmetries = ",symmetries
+symmetries = ['1A1','1B1','3B1','1A2','3A2','1E','3E']
+print ("compute A(w) for symmetries = ",symmetries)
+